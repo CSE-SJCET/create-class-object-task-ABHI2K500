6476481e18f33972f19b01da4684fb32d54d5fdb
ABHIJITH P <183481970+ABHI2K500@users.noreply.github.com>
2025-07-28 17:54:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
-// Student class with name, rollNumber, and grade
 class Student {
     String name;
     int rollNumber;
     char grade;
 
-    // Constructor to initialize student details
     Student(String name, int rollNumber, char grade) {
         this.name = name;
         this.rollNumber = rollNumber;
         this.grade = grade;
     }
 
-    // Method to display student details
-    void displayDetails() {
+    public void displayDetails() {
         System.out.println("Name: " + name);
         System.out.println("Roll Number: " + rollNumber);
         System.out.println("Grade: " + grade);
     }
 }
-
 public class Main {
     public static void main(String[] args) {
-        // Create a Student object and display details
         Student student = new Student("Alex", 101, 'A');
         student.displayDetails();
     }
